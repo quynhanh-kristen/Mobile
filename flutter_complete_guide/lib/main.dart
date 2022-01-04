a75f90a0bb19a7f7e95e5400ab851e3d83ab94cb
DELL <anhnpqse140645@gmail.com>
2022-01-04 16:57:12 +0700

    adding transaction model and transaction data

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,90 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/result.dart';
+import './transaction.dart';
 
-import './quiz.dart';
+void main() => runApp(MyApp());
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return MyAppState();
-  }
-}
-
-class MyAppState extends State<MyApp> {
-  var _questions =  [
-    {
-      'questionText':'What\'s your favorite color ?',
-     'answers': [
-       {'text':'Black', 'score': 5},
-       {'text':'Red', 'score': 4},
-       {'text':'Green', 'score': 3}, 
-       {'text':'White', 'score': 2}
-       ]
-     },
-    {
-      'questionText':'What\'s your favorite animal ?',
-     'answers': [
-       {'text':'Monkey', 'score': 4},
-       {'text':'Dog', 'score': 3},
-       {'text':'Cat', 'score': 2}, 
-       {'text':'Howl', 'score': 1}
-       ]
-     },
-     {
-      'questionText':'What\'s your favorite food ?',
-     'answers': [
-       {'text':'Eggs', 'score': 4},
-       {'text':'Banana', 'score': 3},
-       {'text':'Potato', 'score': 2}, 
-       {'text':'Avocado', 'score': 1}
-       ]
-     },
-    
-  ];
-  //questions = [] -> does not work if questions is a const
-  var questionIndex = 1;
-  var _totalScore = 1;
-
-  void _resetQuiz(){
-    setState(() {
-      questionIndex = 0;
-    _totalScore = 0;
-    });
-    
-  }
-
-  void _answerQuestion(int score) {
-
-    _totalScore += score;
-    setState(() {
-      questionIndex = questionIndex + 1;
-    });
-    if (questionIndex < _questions.length) {
-      print('We have more questions: ');
-    }else{
-      print('No more question');
-    }
-
-    print('Question Index: ' + questionIndex.toString());
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My nst app'),
-        ),
-        body: questionIndex < _questions.length
-        ? Quiz(
-          answerQuestion: _answerQuestion,
-          questionIndex: questionIndex,
-          questions: _questions,)
-        : Result(_totalScore, _resetQuiz),
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART!'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            color: Colors.red,
+            child: Text('LIST OF TX'),
+          ),
+        ],
       ),
     );
   }
